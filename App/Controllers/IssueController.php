d043035376d7f5a288b8a02eabbbcbae695f2d1c
Zaid Bari <hello@itszbari.com>
2023-04-11 05:00:39 +0300

    article view

--- App/Controllers/IssueController.php
+++ App/Controllers/IssueController.php
@@ -111,16 +111,9 @@ class IssueController extends Controller
     {
 
         // $content = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/files_html/'. "issue.html");
-        // $tags = "<!DOCTYPE html><html><head></head><body>". $content. "</body></html>";
-        // $con = $this->closetags($tags);
-        // file_put_contents($_SERVER['DOCUMENT_ROOT'].'/files_html/'. "issue_fixed.html", $con);
-        // $contents = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/files_html/' . "issue_fixed.html");
         
         $content = file_get_contents("https://www.ejmanager.com/index_myjournal.php?jid=" . $_ENV["JOURNAL_ID"]. "&sec=cissue");
-        $tags = "<!DOCTYPE html><html><head></head><body>". $content. "</body></html>";
-        $con = $this->closetags($tags);
-        file_put_contents($_SERVER['DOCUMENT_ROOT']."/files_html/". "issue.html", $con);
-        $contents = file_get_contents($_SERVER['DOCUMENT_ROOT']."/files_html/" . "issue.html");
+        $contents = str_replace("Ã¢&#x80;&#x99;", "'", $this->closetags($content));
         
         
         $data = [];
