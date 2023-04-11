7e24046dd98fe40e5060e3223a32d9a5bc969fbf
Zaid Bari <hello@itszbari.com>
2023-04-11 04:43:03 +0300

    article view

--- App/Controllers/IssueController.php
+++ App/Controllers/IssueController.php
@@ -117,7 +117,7 @@ class IssueController extends Controller
         // $contents = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/files_html/' . "issue_fixed.html");
         
         $content = file_get_contents("https://www.ejmanager.com/index_myjournal.php?jid=" . $_ENV["JOURNAL_ID"]. "&sec=cissue");
-        $con = $this->closetags($content);
+        $con = $this->closetags(htmlentities($content, ENT_QUOTES));
         file_put_contents($_SERVER['DOCUMENT_ROOT'].'/files_html/'. "issue.html", $con);
         $contents = file_get_contents($_SERVER['DOCUMENT_ROOT'].'/files_html/' . "issue.html");
         
