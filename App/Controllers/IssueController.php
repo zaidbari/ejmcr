c92e49cfa016724892699ec450c9db14380351d3
Zaid Bari <hello@itszbari.com>
2023-04-11 04:10:40 +0300

    article view

--- App/Controllers/IssueController.php
+++ App/Controllers/IssueController.php
@@ -9,25 +9,21 @@ class IssueController extends Controller
 
     function closetags($html)
     {
-        preg_match_all('#<([a-z]+)(?: .*)?(?<![/|/ ])>#iU', $html, $result);
-        $openedtags = $result[1];
-        preg_match_all('#</([a-z]+)>#iU', $html, $result);
-    
-        $closedtags = $result[1];
-        $len_opened = count($openedtags);
-    
-        if (count($closedtags) == $len_opened) {
-            return $html;
+        libxml_use_internal_errors(true);
+
+        $dom = new \DOMDocument;
+        $dom->loadHTML($html);
+
+        // Strip wrapping <html> and <body> tags
+        $mock = new \DOMDocument;
+        $body = $dom->getElementsByTagName('body')->item(0);
+        foreach ($body->childNodes as $child) {
+            $mock->appendChild($mock->importNode($child, true));
         }
-        $openedtags = array_reverse($openedtags);
-        for ($i=0; $i < $len_opened; $i++) {
-            if (!in_array($openedtags[$i], $closedtags)) {
-                $html .= '</'.$openedtags[$i].'>';
-            } else {
-                unset($closedtags[array_search($openedtags[$i], $closedtags)]);
-            }
-        }
-        return $html;
+
+        $fixed = trim($mock->saveHTML());
+        
+        return $fixed;
     }
     
     private function extract_issue_url($html)
