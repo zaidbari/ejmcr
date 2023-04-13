96cad3773e19fb4e38d5ce55bbdadf32245552cb
Zaid Bari <hello@itszbari.com>
2023-04-13 16:16:14 +0300

    articles finished

--- App/Controllers/IssueController.php
+++ App/Controllers/IssueController.php
@@ -46,7 +46,7 @@ class IssueController extends Controller
         
         $this->view('issues/index', [
             'meta' => [
-                'title' =>  $data['issue_details']['volume'] .' | Issue ' .  $data['issue_details']['issue'],
+                'title' => 'Volume ' . $data['issue_details']['volume'] .' | Issue ' .  $data['issue_details']['issue'],
                 'description' => $description,
             ],
             "data" => $data
