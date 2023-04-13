a3c6f8503117b162b23d311e38daceea87a9da3a
Zaid Bari <hello@itszbari.com>
2023-04-13 17:23:33 +0300

    articles finished

--- App/Controllers/IssueController.php
+++ App/Controllers/IssueController.php
@@ -62,7 +62,7 @@ class IssueController extends Controller
         $class = "class='alert-primary alert mt-3'";
         // $content = file_get_contents($_SERVER['DOCUMENT_ROOT']. '/files_html/archives_fixed.html');
         $content = str_replace(["&nbsp;", "&raquo;", "&#187;"], "", $content);
-        $content = str_replace(["&amp;&amp;", "&amp;"], "&", $content);
+        $content = str_replace(["&amp;&amp;", "&amp;", "&&"], "&", $content);
         $content = str_replace("style='padding:19px;'", $class, $content);
         $content = str_replace("?iid=", "/issue?iid=", $content);
         $content = str_replace("&jid=" . $_ENV['JOURNAL_ID'] . "&lng=", "", $content);
