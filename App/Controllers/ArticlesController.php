1a887beab53c46ba9f0b6303a3de741d516ad5ca
Zaid Bari <hello@itszbari.com>
2023-04-13 03:50:39 +0300

    Issues finished

--- App/Controllers/ArticlesController.php
+++ App/Controllers/ArticlesController.php
@@ -12,18 +12,18 @@ class ArticlesController extends Controller
     public function index()
     {
 
-        // $content = file_get_contents("https://www.ejmanager.com/index_myjournal.php?jid=" . $_ENV['JOURNAL_ID'] . "&mno=" . $_GET['mno']);
-        // $contents = $this->fixTags(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'));
-        // unset($content);
+        $content = file_get_contents("https://www.ejmanager.com/index_myjournal.php?jid=" . $_ENV['JOURNAL_ID'] . "&mno=" . $_GET['mno']);
+        $contents = $this->fixTags(mb_convert_encoding($content, 'HTML-ENTITIES', 'UTF-8'));
+        unset($content);
 
-        $contents = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/files_html/article_fixed.html');
+        // $contents = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/files_html/article_fixed.html');
         $data = $this->extract_article_data(str_replace("<br>", "", $contents));
         
-        // $info_file = file_get_contents("https://www.ejmanager.com/index_myjournal.php?jid=" . $_ENV['JOURNAL_ID'] . "&mno=" . $_GET['mno'] . "&sec=articleInfo");
-        // $info_contents = $this->fixTags(mb_convert_encoding($info_file, 'HTML-ENTITIES', 'UTF-8'));
-        // unset($info_file);
+        $info_file = file_get_contents("https://www.ejmanager.com/index_myjournal.php?jid=" . $_ENV['JOURNAL_ID'] . "&mno=" . $_GET['mno'] . "&sec=articleInfo");
+        $info_contents = $this->fixTags(mb_convert_encoding($info_file, 'HTML-ENTITIES', 'UTF-8'));
+        unset($info_file);
 
-        $info_contents = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/files_html/article_info.html');
+        // $info_contents = file_get_contents($_SERVER['DOCUMENT_ROOT'] . '/files_html/article_info.html');
         $article_info = $this->extract_article_info($info_contents);
 
         $this->view('articles/index', [
